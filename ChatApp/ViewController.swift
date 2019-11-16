21b5d257ad0a3120403f261d2e3193d4e856decb
Gavin Brown <Scareb15@gmail.com>
2019-11-16 12:00:41 -0600

    Initial Commit

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Gavin Brown on 11/16/19.
+//  Copyright © 2019 DevelopITLLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
