4820aa3f33fb4826f90503b297dcd5d61fb51c8b
Gavin Brown <Scareb15@gmail.com>
2019-11-16 12:18:23 -0600

    added ReadMe

--- ChatApp/ViewController.swift
+++ ChatApp/ViewController.swift
@@ -8,11 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleLogout))
+    }
+    
+    
+    @objc func handleLogout() {
+        print("we have logged out")
     }
 
 
---
+++ README.md
@@ -0,0 +1,9 @@
+#Chat App
+---
+##Purpose
+
+- To create something fun and useful that i could reference in the future when building other chat based apps
+- Showcase skill set in regards to swift language
+- Using tools like **Firebase** in a way i have not before
+- Gain more experience creating apps natively
+- Did i mention for fun?
